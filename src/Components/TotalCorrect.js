d7eb9635a9eef678afa13e7a2f65b38b27343bac
shaikhhira <shaikhhira393@gmail.com>
2020-12-12 21:06:38 +0500

    Updates

--- src/Components/TotalCorrect.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function TotalCorrect(props) {
-  return <p className="correct">Correct: {props.correct}</p>;
-}
-export default TotalCorrect;
--- src/Components/TotalIncorrect.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function TotalIncorrect(props) {
-  return <p className="incorrect">Incorrect: {props.incorrect}</p>;
-}
-export default TotalIncorrect;
